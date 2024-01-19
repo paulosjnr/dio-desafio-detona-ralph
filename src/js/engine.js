551a189209f6dc98fba6de8fc1989950c57889cd
paulosjnr <108819557+paulosjnr@users.noreply.github.com>
2024-01-19 17:35:38 -0300

    adicionando script e a mecanica do jogo

---
+++ src/js/engine.js
@@ -0,0 +1,39 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+
+    values: {
+        gameSpeed: 1000,
+        timerID: null
+    },
+};
+
+function randomSquare (){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+}
+
+function moveEnemy(){
+    state.values.timerID = setInterval(randomSquare, state.values.gameSpeed);
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+        
+    });
+}
+
+function main() {
+    moveEnemy();
+}
+
+main();
